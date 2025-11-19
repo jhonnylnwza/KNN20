18d53c60b8d397720859b75b27ce7709fce44ee5
Johnnylnwza <664230020@webmail.npru.ac.th>
2025-11-19 15:37:43 +0700

    .

---
+++ Home.py
@@ -0,0 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+st.header('kim')
+st.img('./img/kim.jpeg')
+col1, col2, col3 = st.columns(3)
+
