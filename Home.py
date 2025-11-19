0d55800abf002b7be054aa0dcb9816fc8e959c80
Johnnylnwza <664230020@webmail.npru.ac.th>
2025-11-19 15:40:34 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,5 +6,17 @@ import plotly.graph_objects as go
 
 st.header('kim')
 st.image("./img/kim.jpeg")
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
 
